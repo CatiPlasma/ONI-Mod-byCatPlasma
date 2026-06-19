54799ca0c97b6cac54527e592c74562232a3e67a
CatPlasma <110224489+CatiPlasma@users.noreply.github.com>
2026-06-19 17:03:14 +0800

    Add Log Methods

---
+++ CatUtilLib/LogUtil.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Reflection;
+using System.Threading;
+
+namespace CatUtilLib
+{
+    public static class LogUtil
+    {
+        public static void Info(string msg)
+        {
+            Console.WriteLine($"{CurrentTIme()} [INFO] [{Assembly.GetCallingAssembly().GetName().Name}]: {msg}");
+        }
+
+        public static void Debug(string msg)
+        {
+            Console.WriteLine($"{CurrentTIme()} [DEBUG] [{Assembly.GetCallingAssembly().GetName().Name}]: {msg}");
+        }
+
+        private static string CurrentTIme()
+        {
+            return
+                $"[{TimeZoneInfo.ConvertTimeToUtc(System.DateTime.Now).ToString("HH:mm:ss.fff")}] [{Thread.CurrentThread.ManagedThreadId}]";
+        }
+    }
+}
