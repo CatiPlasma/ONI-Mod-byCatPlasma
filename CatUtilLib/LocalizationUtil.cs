4230c8fd79c857872633e48c6d7bf7a7c57f279a
CatPlasma <110224489+CatiPlasma@users.noreply.github.com>
2026-06-20 00:07:08 +0800

    Add Localization Tool

---
+++ CatUtilLib/LocalizationUtil.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace CatUtilLib
+{
+    public class LocalizationUtil
+    {
+        public static void MakeLocalization(Type root)
+        {
+            string modPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            string transPath = Path.Combine(modPath, "Translations");
+            string poPath = Path.Combine(transPath, Localization.GetLocale()?.Code ?? "en", ".po");
+            
+            if (!Directory.Exists(transPath)) Directory.CreateDirectory(transPath);
+            Localization.RegisterForTranslation(root);
+            LocString.CreateLocStringKeys(root, null);
+            Localization.GenerateStringsTemplate(root, transPath);
+            if (File.Exists(poPath)) Localization.OverloadStrings(Localization.LoadStringsFile(poPath, false));
+        }
+    }
+}
