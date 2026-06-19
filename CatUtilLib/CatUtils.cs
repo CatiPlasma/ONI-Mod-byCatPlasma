481a4c4a71e8504c35bc07fa7b4bc1d95a325ceb
CatPlasma <110224489+CatiPlasma@users.noreply.github.com>
2026-06-19 20:06:57 +0800

    Add single roll method

--- CatUtilLib/CatUtils.cs
+++ CatUtilLib/CatUtils.cs
@@ -10,9 +10,25 @@ namespace CatUtilLib
             return Global.Instance.modManager.mods.Any(mod => mod.staticID == modId && mod.IsActive());
         }
         
+        public static float[] Roll(GameObject gameObject, float min, float max, int count)
+        {
+            float[] result = new float[count];
+            
+            if (SaveLoader.Instance == null || SaveLoader.Instance.clusterDetailSave == null)
+            {
+                for (int i=0; i<count; i++) result[i] = -1f;
+                return result;
+            }
+            
+            KRandom randomSource =  new KRandom(SaveLoader.Instance.clusterDetailSave.globalWorldSeed + (int)gameObject.transform.GetPosition().x + (int)gameObject.transform.GetPosition().y);
+            for (int i=0; i<count; i++) result[i] = (float)(randomSource.NextDouble() * (max - min)) + min;
+            return result;
+        }
+        
         public static float Roll(GameObject gameObject, float min, float max)
         {
             if (SaveLoader.Instance == null || SaveLoader.Instance.clusterDetailSave == null) return -1f;
+            
             KRandom randomSource =  new KRandom(SaveLoader.Instance.clusterDetailSave.globalWorldSeed + (int)gameObject.transform.GetPosition().x + (int)gameObject.transform.GetPosition().y);
             return (float)(randomSource.NextDouble() * (max - min)) + min;
         }
