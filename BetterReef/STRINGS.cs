8ee1c8cd2747429a35ddc073311d45709e1ada85
CatPlasma <110224489+CatiPlasma@users.noreply.github.com>
2026-06-20 18:43:25 +0800

    Update strings file

--- BetterReef/STRINGS.cs
+++ BetterReef/STRINGS.cs
@@ -12,7 +12,7 @@
                 public static LocString TIDALPEKGEN = "Peak Power Generation: {0}";
                 public static LocString TIDALAVGGEN = "Average Power Generation: {0}";
 
-                public static LocString UWATERVENTEMIT = "Emitting Rate: {0}";
+                public static LocString UWATERVENTEMIT = "Emitting Rate: {0}/s";
                 public static LocString UWATERVENTDURATION = "Build-up Time: {0}";
                 public static LocString UWATERVENTSOLID = string.Concat(new string[]
                 {
