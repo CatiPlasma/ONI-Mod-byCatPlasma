ce76907257f1c7c3b62a12570172b4dd3d0f7335
CatPlasma <110224489+CatiPlasma@users.noreply.github.com>
2026-06-22 22:48:02 +0800

    Update method Roll

--- CatUtilLib/CatUtils.cs
+++ CatUtilLib/CatUtils.cs
@@ -10,7 +10,7 @@ namespace CatUtilLib
             return Global.Instance.modManager.mods.Any(mod => mod.staticID == modId && mod.IsActive());
         }
         
-        public static float[] Roll(GameObject gameObject, float min, float max, int count)
+        public static float[] RollFloats(GameObject gameObject, float min, float max, int count)
         {
             float[] result = new float[count];
             
@@ -25,12 +25,60 @@ namespace CatUtilLib
             return result;
         }
         
-        public static float Roll(GameObject gameObject, float min, float max)
+        public static float RollFloats(GameObject gameObject, float min, float max)
         {
-            if (SaveLoader.Instance == null || SaveLoader.Instance.clusterDetailSave == null) return -1f;
+            return RollFloats(gameObject, min, max, 1)[0];
+        }
+
+        public static int[] RollInts(GameObject gameObject, int min, int max, int count)
+        {
+            int[] result = new int[count];
+
+            if (SaveLoader.Instance == null || SaveLoader.Instance.clusterDetailSave == null)
+            {
+                for (int i=0; i<count; i++)  result[i] = -1;
+                return result;
+            }
             
-            KRandom randomSource =  new KRandom(SaveLoader.Instance.clusterDetailSave.globalWorldSeed + (int)gameObject.transform.GetPosition().x + (int)gameObject.transform.GetPosition().y);
-            return (float)(randomSource.NextDouble() * (max - min)) + min;
+            KRandom randomSource = new KRandom(SaveLoader.Instance.clusterDetailSave.globalWorldSeed + (int)gameObject.transform.GetPosition().x + (int)gameObject.transform.GetPosition().y);
+            for (int i = 0; i < count; i++) result[i] = randomSource.Next() * (max - min) + min;
+            return result;
+        }
+
+        public static int RollInts(GameObject gameObject, int min, int max)
+        {
+            return RollInts(gameObject, min, max, 1)[0];
+        }
+        
+        public static bool HasWorldTrait(GameObject go, string traitId)
+        {
+            WorldContainer world = go.GetMyWorld();
+            if (world == null) return false;
+            return world.WorldTraitIds.Contains(traitId);
+        }
+
+        public static bool HasBiome(GameObject go, string biomeId)
+        {
+            foreach (string biome in go.GetMyWorld().Biomes)
+            {
+                if (biomeId == biome) return true;
+            }
+
+            return false;
+        }
+
+        public static void LogWorldTraits(int worldId)
+        {
+            WorldContainer world = ClusterManager.Instance.GetWorld(worldId);
+            if (world == null) return;
+            foreach (string id in world.WorldTraitIds) LogUtil.Debug($"WorldTraitId: {id}");
+        }
+
+        public static void LogBiomeTraits(int worldId)
+        {
+            WorldContainer world = ClusterManager.Instance.GetWorld(worldId);
+            if (world == null) return;
+            foreach (string id in world.Biomes) LogUtil.Debug($"BiomeId: {id}");
         }
     }
 }
